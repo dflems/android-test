e466c4ae3478574b881b106aea9e0836759f1ef1
AndroidX Test Team <copybara-androidxtest@google.com>
2022-02-04 12:51:51 -0800

    Synchronize setting emulator to flat mode
    PiperOrigin-RevId: 426470772

--- espresso/core/java/androidx/test/espresso/device/action/FlatModeAction.kt
+++ espresso/core/java/androidx/test/espresso/device/action/FlatModeAction.kt
@@ -16,17 +16,73 @@
 
 package androidx.test.espresso.device.action
 
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import androidx.core.util.Consumer
 import androidx.test.espresso.device.context.ActionContext
+import androidx.test.espresso.device.controller.DeviceControllerOperationException
 import androidx.test.espresso.device.controller.DeviceMode
+import androidx.test.espresso.device.util.getResumedActivityOrNull
+import androidx.test.espresso.device.util.isRobolectricTest
 import androidx.test.platform.device.DeviceController
+import androidx.window.java.layout.WindowInfoTrackerCallbackAdapter
+import androidx.window.layout.FoldingFeature
+import androidx.window.layout.WindowInfoTracker
+import androidx.window.layout.WindowLayoutInfo
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executor
 
 /** Action to set the test device to be completely flat, like a tablet. */
 internal class FlatModeAction() : DeviceAction {
+  companion object {
+    private val TAG = "FlatModeAction"
+
+    private class WindowLayoutInfoConsumer(
+      private val latch: CountDownLatch,
+      private val windowInfoTrackerCallbackAdapter: WindowInfoTrackerCallbackAdapter
+    ) : Consumer<WindowLayoutInfo> {
+      override fun accept(windowLayoutInfo: WindowLayoutInfo) {
+        windowLayoutInfo.displayFeatures.filterIsInstance<FoldingFeature>().forEach {
+          if (it.state == FoldingFeature.State.FLAT) {
+            Log.d(TAG, "Device is in flat mode")
+            windowInfoTrackerCallbackAdapter.removeWindowLayoutInfoListener(this)
+            latch.countDown()
+          }
+        }
+      }
+    }
+  }
+
   override fun perform(context: ActionContext, deviceController: DeviceController) {
     // TODO(b/203801760): Check current device mode and return if already in flat mode.
+    if (isRobolectricTest()) {
+      deviceController.setDeviceMode(DeviceMode.FLAT.mode)
+      return
+    }
+
+    val activity =
+      getResumedActivityOrNull()
+        ?: throw DeviceControllerOperationException(
+          "Unable to set device to flat mode because there are no activities in the resumed stage."
+        )
+    val executor: Executor = Executor { command -> Handler(Looper.getMainLooper()).post(command) }
+    val latch: CountDownLatch = CountDownLatch(1)
+    val windowInfoTrackerCallbackAdapter =
+      WindowInfoTrackerCallbackAdapter(WindowInfoTracker.getOrCreate(activity))
+    val windowLayoutInfoConsumer: Consumer<WindowLayoutInfo> =
+      WindowLayoutInfoConsumer(
+        latch,
+        windowInfoTrackerCallbackAdapter,
+      )
+
+    windowInfoTrackerCallbackAdapter.addWindowLayoutInfoListener(
+      activity,
+      executor,
+      windowLayoutInfoConsumer
+    )
 
     deviceController.setDeviceMode(DeviceMode.FLAT.mode)
-
-    // TODO(b/203801783): Synchronize device controller call.
+    latch.await()
   }
 }
